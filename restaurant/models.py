cf68cdad74b55b3776a6d840ab80da9c727af0f1
Mollis <himuchik09@gmail.com>
2024-10-15 10:01:56 +0900

    models.py 필드 수정

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,7 +10,10 @@ class Chef(models.Model):
 class Restaurant(models.Model):
     chef = models.ForeignKey(Chef, on_delete=models.CASCADE, related_name='restaurants')  # 쉐프와 연결
     restaurant_name = models.CharField(max_length=100, verbose_name='레스토랑명', null=False)
-    
+    address = models.CharField(max_length=200, verbose_name='주소', null=False)
+    style = models.CharField(max_length=200, verbose_name='스타일', null=False)
+    url = models.CharField(max_length=100, verbose_name='catchtable_url', null=False)
+    review_count = models.BigIntegerField(verbose_name='리뷰수', null=False)
 
     def __str__(self):
         return f'쉐프 : {self.chef.chef_name} / 레스토랑 : {self.restaurant_name}'
@@ -26,7 +29,7 @@ class Review(models.Model):
 class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menus')  # 레스토랑과 연결
     menu_name = models.CharField(max_length=100, verbose_name='메뉴명', null=False)
-    price = models.BigIntegerField(verbose_name='가격', null=False)
+    price = models.CharField(max_length=50, verbose_name='가격', null=False)
 
     def __str__(self):
         return f'{self.restaurant.restaurant_name} - {self.menu_name} - {self.price} 원'
